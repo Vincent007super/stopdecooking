9fe495f2ac158cb2787ee85102920b4bee7e393e
Nigel <Nigel.becholtz@gmail.com>
2024-09-25 16:08:51 +0200

    login page

---
+++ login.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'views/login_view.php';
---
+++ views/login_view.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/login.css">
+    <title>Login</title>
+</head>
+<body>
+<div class="container">
+    <h1>Welcome</h1>
+    <form>
+        <div class="input-container">
+            <label for="email"></label><input type="email" id="email" name="email" placeholder="Email address*" required>
+        </div>
+        <button type="submit">Continue</button>
+    </form>
+</div>
+</body>
+</html>
