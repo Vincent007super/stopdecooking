fe40972ceaba6c56b3c42b7b60fc03e1d65670e4
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2024-11-13 14:18:01 +0100

    Updated login page
    - Added background image to login.php

--- views/login_view.php
+++ views/login_view.php
@@ -28,6 +28,8 @@
     </div>
 </div>
 
+<div class="background"></div>
+
 <!-- Bootstrap JS voor interactiviteit -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
